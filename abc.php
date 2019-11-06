0093b5c6d2b78200aac9d174b3173895c365bcc4
dassuptirani <55455884+dassuptirani@users.noreply.github.com>
2019-11-06 12:55:41 +0600

    Add files via upload

---
+++ abc.php
@@ -0,0 +1,18 @@
+<?php
+
+	$uname			=$_REQUEST['Uname'];
+	$password		=$_REQUEST['Pword'];
+	$DateOfBirth	=$_REQUEST['date'];
+	$Add			=$_REQUEST['add'];
+    $Dept			=$_REQUEST['dept'];
+    $Gender			=$_REQUEST['gender'];
+	
+	echo $uname ;
+	echo $password ;
+	echo $DateOfBirth ;
+	echo $Add ;
+	echo $Dept ;
+	echo $Gender ;
+	
+
+?>
